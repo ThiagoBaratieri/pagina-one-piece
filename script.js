45357c2a09d5fcdf4e51d05e7abd43c572fa7a0e
Thiago Baratieri <thiagobaratieri971@gmail.com>
2023-09-25 21:16:26 -0300

    Criação de um botão para download / ALterações CSS - Html

--- script.js
+++ script.js
@@ -45,4 +45,32 @@ window.addEventListener('load', function () {
       slider.style.transform = `translateX(${translateX}px)`;
   }
 
+  /* Configurações botão de download */
+  const downloadButton = document.getElementById('download-button');
+
+  downloadButton.addEventListener('click', () => {
+    const sliderImg = document.querySelector('.slider');
+    const imagens = sliderImg.querySelectorAll('img');
+    const nomesDaImg = ['img-one-piece1', 'img-one-piece2', 'img-one-piece3'];
+
+    const imagePath = imagens[currentIndex].getAttribute('src');
+
+    if (imagePath) {
+      const nomeDaImg = nomesDaImg[currentIndex];
+      const downloadLink = document.createElement('a');
+      
+      downloadLink.href = imagePath
+      downloadLink.download = nomeDaImg;
+      document.body.appendChild(downloadLink);
+
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+    } else {
+      alert('Nenhuma imagem para baixar');
+    }
+  });
+
+
+
+
 });
