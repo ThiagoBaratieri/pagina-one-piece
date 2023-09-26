dd6fbd2b3b615bf4de02a2f9e879e2f7da0b4071
Thiago Baratieri <thiagobaratieri971@gmail.com>
2023-09-26 03:14:07 -0300

    Lógica principai quiz pronta / config css / alt hmtl

--- script.js
+++ script.js
@@ -69,8 +69,148 @@ window.addEventListener('load', function () {
       alert('Nenhuma imagem para baixar');
     }
   });
+});
 
 
+
+document.addEventListener('DOMContentLoaded', function () {
+  /* Declarando Variáveis */
+  const telaInicial = document.getElementById('tela-inicial');
+  const telaQuiz = document.getElementById('tela-quiz');
+  const resultadoQuiz = document.getElementById('resultado-quiz');
+  const comecarQuizButton = document.getElementById('comecar-quiz');
+  const marcadorPergunta = document.getElementById('marcador-pergunta');
+  const perguntaConteiner = document.getElementById('pergunta-conteiner');
+  const perguntaElemento = document.getElementById('pergunta');
+  const opcoesElemento = document.getElementById('opcoes');
+  const refazerButton = document.getElementById('refazer-botao');
+  const respostasCorretas = document.getElementById('respostas-corretas');
+
+  /* Lista com as perguntas */
+  const perguntas = [
+    {
+        pergunta: 'Quem é o capitão dos Piratas do Chapéu de Palha?',
+        opcoes: ['Luffy', 'Zoro', 'Law', 'Sanji'],
+        resposta: 'Luffy'
+    },
+    {
+        pergunta: 'Qual é a fruta do diabo de Luffy?',
+        opcoes: ['Fogo-Fogo', 'Gomu-Gomu', 'Bara-Bara', 'Yami-Yami'],
+        resposta: 'Gomu-Gomu'
+    },
+    {
+        pergunta: 'Qual é o nome do navio dos Piratas do Chapéu de Palha?',
+        opcoes: ['Going Merry', 'Thousand Sunny', 'Red Force', 'Going Sunny'],
+        resposta: 'Thousand Sunny'
+    }
+  ];
+  
+  let perguntaAtual = 0;
+  let marcadorAtual = 0;
+  let acertos = 0;
+
+  comecarQuizButton.addEventListener('click', function () {
+    console.log('Apertou para começar o QUIZ', comecarQuizButton);
+    // Ocultar tela inicial
+    telaInicial.style.display = 'none';
+
+    // Mostrar tela do quiz
+    telaQuiz.style.display = 'block';
+
+    // Iniciar o quiz
+    iniciarQuiz();
+  });
+
+  function iniciarQuiz() {
+    exibirPergunta();
+}
+
+  function reiniciarQuiz() {
+    perguntaAtual = 0;
+    marcadorAtual = 0;
+    exibirPergunta(); // Exibe a primeira pergunta ao reiniciar o quiz
+  }
+
+  function exibirPergunta() {
+    const pergunta = perguntas[perguntaAtual];
+    perguntaElemento.textContent = pergunta.pergunta;
+
+    // Limpe as opções anteriores
+    opcoesElemento.innerHTML = '';
+
+    // Exiba as opções
+    pergunta.opcoes.forEach((opcao) => {
+      const opcaoElemento = document.createElement('div');
+      opcaoElemento.textContent = opcao;
+      opcaoElemento.classList.add('opcao');
+      opcaoElemento.addEventListener('click', () => verificarResposta(opcao));
+      opcoesElemento.appendChild(opcaoElemento);
+    });
+
+    // Atualize o marcador de pergunta
+    marcadorPergunta.textContent = `${perguntaAtual + 1} / ${perguntas.length}`;
+  }
+
+  function verificarResposta(respostaSelecionada) {
+    const respostaCorreta = perguntas[perguntaAtual].resposta;
+
+    if (respostaSelecionada === respostaCorreta) {
+        // Resposta correta
+        perguntaAtual++;
+        marcadorAtual++;
+        acertos++;
+
+        if (perguntaAtual < perguntas.length) {
+            exibirPergunta();
+        } else {
+            // Fim do quiz
+            telaQuiz.style.display = 'none';
+            resultadoQuiz.style.display = 'block';
+
+            opcoesElemento.innerHTML = '';
+            marcadorPergunta.textContent = '';
+
+            refazerButton.addEventListener('click', function () {
+              // Ocultar tela resultado
+              resultadoQuiz.style.display = 'none';
+          
+              // Mostrar tela do quiz
+              telaQuiz.style.display = 'block';
+          
+              // Iniciar o quiz
+              reiniciarQuiz()
+            });
+
+          }
+    } else {
+        // Resposta errada (você pode adicionar lógica adicional aqui)
+        perguntaAtual++;
+        marcadorAtual++;
+        
+        if (perguntaAtual < perguntas.length) {
+          exibirPergunta();
+        } else {
+            // Fim do quiz
+            telaQuiz.style.display = 'none';
+            resultadoQuiz.style.display = 'block';
+
+            opcoesElemento.innerHTML = '';
+            marcadorPergunta.textContent = '';
+
+            refazerButton.addEventListener('click', function () {
+              // Ocultar tela resultado
+              resultadoQuiz.style.display = 'none';
+          
+              // Mostrar tela do quiz
+              telaQuiz.style.display = 'block';
+          
+              // Iniciar o quiz
+              reiniciarQuiz();
+            });
+
+        }
+      }
+  }
 
 
 });
